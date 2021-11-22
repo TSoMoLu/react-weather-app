273d0c2bf5a6ecf51dc02da64fe1747bface2b8b
TSoMoLu <87667797+TSoMoLu@users.noreply.github.com>
2021-11-22 20:23:53 +0100

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Tanimola Somolu and is{" "}
+        <a href="https://github.com/TSoMoLu/react-weather-app" target="_blank">
+          open-sourced
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
 
 export default App;
